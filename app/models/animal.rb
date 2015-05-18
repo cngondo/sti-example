4f25c07c2e8941ecd4ad07e316c2e1b053039dce
Cornellius Ngondo <devngondo@gmail.com>
2015-05-18 18:42:11 +0000

    Added submodels

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,10 @@
 class Animal < ActiveRecord::Base
-	
+	belongs_to :tribe 
+    self.inheritance_column = :race 
+
+    # We will need a way to know which animals
+    # will subclass the Animal model
+    def self.races
+      %w(Lion WildBoar Meerkat)
+    end	
 end
